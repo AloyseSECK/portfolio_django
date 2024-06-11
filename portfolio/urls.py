2b071b45834aeaecdfbf1c983f3f5d0be453924e
AloyseSECK <seckalowiz@gmail.com>
2024-06-12 00:00:01 +0200

    Set home2 as default page

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('home2/', views.home2, name='home2'),
+    # path('', views.home, name='home'),
+    path('', views.home2, name='home'),
     path('cv/', views.render_xml, name='render_xml'),
 ]
 

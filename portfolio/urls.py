aceccfe116e339be175abd9e8e18c06de08f8044
AloyseSECK <seckalowiz@gmail.com>
2024-06-11 14:28:54 +0200

    ADD : styled_cv url

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('cv/', views.render_xml, name='render_xml'),
+    path('cv-styled/', views.render_styled_cv, name='render_styled_cv'),
 ]
 

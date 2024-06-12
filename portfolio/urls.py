c94fdf658fc30d18ab77239ff3042c795e61577e
AloyseSECK <seckalowiz@gmail.com>
2024-06-12 12:59:24 +0200

    Render html

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('cv/', views.render_xml, name='render_xml'),
+    path('cv/', views.render_html, name='render_html'),
 ]
 

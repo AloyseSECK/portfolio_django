d9ba718c410fb27a6fd94524b8fb975cb01169e8
AloyseSECK <seckalowiz@gmail.com>
2026-01-19 11:46:27 +0100

    debug false and add allowed hosts

--- portfolio_django/settings.py
+++ portfolio_django/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_)d^=@kj_vy=sl7*e++7cp-tl56d$xuysnn2ob_)w$hez-qd&3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [".vercel.app","127.0.0.1","localhost"]
+ALLOWED_HOSTS = [".vercel.app","127.0.0.1","localhost","aloysemaliamseck.site","www.aloysemaliamseck.site"]
 
 
 # Application definition    
